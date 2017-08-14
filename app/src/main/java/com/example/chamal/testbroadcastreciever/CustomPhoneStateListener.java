da603866f7356d4d224d3f8fcf464df8084f1d55
Chamal Gunasinghe <Chamal Gunasinghe>
2017-08-14 21:59:17 +0530

    Test release

--- app/src/main/java/com/example/chamal/testbroadcastreciever/CustomPhoneStateListener.java
+++ app/src/main/java/com/example/chamal/testbroadcastreciever/CustomPhoneStateListener.java
@@ -1,91 +1,199 @@
 package com.example.chamal.testbroadcastreciever;
 
+
 import android.app.Dialog;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.provider.ContactsContract;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.VideoView;
+
+import com.example.chamal.testbroadcastreciever.Model.Contact;
+import com.example.chamal.testbroadcastreciever.Sqlite.MyDatabaseAdapter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
 
 
 public class CustomPhoneStateListener extends PhoneStateListener {
-        Context context;
-        boolean isRinging;
-        private static int lastState = TelephonyManager.CALL_STATE_IDLE;
+
+    Context context;
+    private static int lastState = -1;
+    AudioManager am;
+    Dialog dialog;
+    private static int ringMode;
+    private boolean isDialogShowing;
+    int curVolume;
+    int curMusicStreamVolume = 0;
+    MyDatabaseAdapter adapter;
+    Map<String, Integer> map;
+    int count = 0;
+    boolean isPrefEnabled;
+    Realm realm;
+
 
     public CustomPhoneStateListener(Context context) {
-        super();
-        this.context = context;
-        isRinging = false;
+
+            this.context = context;
+            isDialogShowing = false;
+            am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+            adapter = new MyDatabaseAdapter(context);
+            map = new HashMap<String, Integer>();
+            map.put("trailer", R.raw.trailer);
+            map.put("trailer02", R.raw.trailer02);
+            map.put("trailer03", R.raw.trailer03);
+
     }
 
     @Override
     public void onCallStateChanged(int state, String incomingNumber) {
 
-        if(lastState == state){
-            return;
-        }
-        switch (state) {
-            case TelephonyManager.CALL_STATE_RINGING:
-                isRinging = true;
-                showDialog(context, incomingNumber);
-                break;
-            case TelephonyManager.CALL_STATE_OFFHOOK:
-                isRinging = false;
-                break;
-        }
+        //Check whether video playback preference has enabled
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        isPrefEnabled = sharedPreferences.getBoolean("prefEnabled", true);
 
+        System.out.println("The state is: "+state);
+
+        switch (state) {
+            case TelephonyManager.CALL_STATE_RINGING:// 1 when 4n is ringing
+
+//                if (lastState != state && isPrefEnabled && count == 0) {
+                if (lastState != state && isPrefEnabled && lastState != TelephonyManager.CALL_STATE_OFFHOOK) {
+                    ringMode = am.getRingerMode();
+                    curMusicStreamVolume = am.getStreamVolume(AudioManager.STREAM_MUSIC);
+                    curVolume = am.getStreamVolume(AudioManager.STREAM_RING);
+                    am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+                    showDialog(context, incomingNumber, false);
+                    isDialogShowing = true;
+                }
+                break;
+            case TelephonyManager.CALL_STATE_IDLE: // 0 When 4n is in the idle mode
+                count = 0;
+                if (isDialogShowing) {
+                    dialog.dismiss();
+                    isDialogShowing = false;
+                    am.setRingerMode(ringMode);
+                    am.setStreamVolume(AudioManager.STREAM_MUSIC, curMusicStreamVolume, 0);
+                }
+                break;
+            case TelephonyManager.CALL_STATE_OFFHOOK:// 2 Take a call & answer a incoming call
+                if (lastState != state && count == 0 && isPrefEnabled && lastState != TelephonyManager.CALL_STATE_RINGING) {
+                    ringMode = am.getRingerMode();
+                    showDialog(context, "", true);
+                    isDialogShowing = true;
+                }
+                break;
+        }
         lastState = state;
     }
 
-    public void showDialog(Context context, String number){
-//        final Dialog dialog = new Dialog(context,android.R.style.Theme_Black_NoTitleBar_Fullscreen);
-        final Dialog dialog = new Dialog(context);
+    public void showDialog(Context context, String number, boolean out) {
+        String videoPath, videoName = null;
+//        dialog = new Dialog(context, android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+        dialog = new Dialog(context);
         dialog.setCancelable(true);
-//        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        String contact = getContactName(context, number);
-        dialog.setTitle(contact);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.dialog);
+        TextView tvCaller = (TextView) dialog.findViewById(R.id.tvCaller);
+
+        if (!out) {
+            String contact = getContactName(context, number);
+
+            if (contact == null) {
+//            dialog.setTitle(number+"  calling...");
+                tvCaller.setText(number + "  calling...");
+                count = 0;
+            } else {
+//            dialog.setTitle(contact+" calling...");
+                tvCaller.setText(contact + " calling...");
+                count = 0;
+            }
+        }
+        if (out) {
+            count = 122;
+            tvCaller.setText("Outgoing call..");
+        }
 
         WindowManager.LayoutParams wmlp = dialog.getWindow().getAttributes();
-
-        wmlp.gravity = Gravity.TOP | Gravity.LEFT;
+        wmlp.gravity = Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL;
         wmlp.x = 1;   //x position
         wmlp.y = 1;   //y position
 
-
-
-        final VideoView videoView = (VideoView)dialog.findViewById(R.id.videoView);
+        final VideoView videoView = (VideoView) dialog.findViewById(R.id.videoView);
         MediaController mediaController = new MediaController(context);
         mediaController.setAnchorView(videoView);
-        String videoPath = "android.resource://com.example.chamal.testbroadcastreciever/"+R.raw.trailer;
-        Uri uri = Uri.parse(videoPath);
+        mediaController.setMediaPlayer(videoView);
+        RealmConfiguration config = new RealmConfiguration.Builder(context).build();
+        Realm.setDefaultConfiguration(config);
+        realm = Realm.getDefaultInstance();
+
+        Contact realmResult;
+
+        Uri uri = null;
+        try {
+            //Get assigned video from db
+            realmResult = realm.where(Contact.class).findFirst();
+            videoName = realmResult.getVideoId();
+
+            uri = Uri.parse(videoName);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         videoView.setMediaController(mediaController);
         videoView.setVideoURI(uri);
+        videoView.requestFocus();
+
+        MediaPlayer.OnPreparedListener PreparedListener = new MediaPlayer.OnPreparedListener() {
+
+            @Override
+            public void onPrepared(MediaPlayer m) {
+
+                if (ringMode == AudioManager.RINGER_MODE_SILENT) {
+                    m.setVolume(0, 0);
+                }
+                if (ringMode == AudioManager.RINGER_MODE_NORMAL) {
+                    am.setStreamVolume(AudioManager.STREAM_MUSIC, curVolume + 5, 0);
+                }
+            }
+        };
+
+        videoView.setOnPreparedListener(PreparedListener);
+
         videoView.start();
 
         videoView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 dialog.dismiss();
+                isDialogShowing = false;
+                am.setRingerMode(ringMode);
                 return true;
             }
         });
-
         dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
         dialog.show();
-//        Toast.makeText(context,"Call from: "+contact, Toast.LENGTH_LONG).show();
     }
 
+
     public static String getContactName(Context context, String phoneNumber) {
+
         ContentResolver cr = context.getContentResolver();
         Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phoneNumber));
         Cursor cursor = cr.query(uri, new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME}, null, null, null);
@@ -93,15 +201,13 @@ public class CustomPhoneStateListener extends PhoneStateListener {
             return null;
         }
         String contactName = null;
-        if(cursor.moveToFirst()) {
+        if (cursor.moveToFirst()) {
             contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
         }
 
-        if(cursor != null && !cursor.isClosed()) {
+        if (cursor != null && !cursor.isClosed()) {
             cursor.close();
         }
-
         return contactName;
     }
-
 }
--- app/src/main/java/com/example/chamal/testbroadcastreciever/IncomingBroadcastReceiver.java
+++ app/src/main/java/com/example/chamal/testbroadcastreciever/IncomingBroadcastReceiver.java
@@ -3,6 +3,9 @@ package com.example.chamal.testbroadcastreciever;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.IBinder;
+import android.preference.PreferenceManager;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 
@@ -12,11 +15,13 @@ import android.telephony.TelephonyManager;
 
 public class IncomingBroadcastReceiver extends BroadcastReceiver {
 
-        @Override
-        public void onReceive(Context context, Intent intent) {
+    @Override
+    public void onReceive(Context context, Intent intent) {
 
-            TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-            telephonyManager.listen(new CustomPhoneStateListener(context), PhoneStateListener.LISTEN_CALL_STATE);
-        }
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        telephonyManager.listen(new CustomPhoneStateListener(context), PhoneStateListener.LISTEN_CALL_STATE);
+
+    }
+
 }
 
